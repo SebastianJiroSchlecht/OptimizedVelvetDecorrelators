7b28c052305ed7cda7c6aef5c195bf6ee578da6b
Sebastian J. Schlecht <sebastianjiroschlecht@gmail.com>
2020-03-09 23:46:53 +0200

    Add evaluation part

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # OptimizedVelvetDecorrelators
 Matlab Code for Schlecht, S., Alary, B., Välimäki, V., Habets, E. (2018). Optimized velvet-noise decorrelator Proc. Int. Conf. Digital Audio Effects (DAFx) 
+
+## Getting started
+The complete experiment can be reproduced by running 
+
+```
+run_completeExperiment
+```
+
+We provide a precomputed and optimized set of 500 sequences. The evaluation part of the experiment can be performed with
+
+```
+run_evaluation
+``` 
---
+++ run_evaluation.m
@@ -0,0 +1,23 @@
+% run complete experiment
+%
+% Sebastian J. Schlecht, Thursday, 20 February 2020
+clear; clc; close all;
+
+startup;
+
+% %% optimization of velvet noise and collect data
+% search_bestVND;
+% collectData;
+% 
+% %% create data set for EVN, OVN30, OVN15, WN (sequence, smooth, error, coherence)
+createSequenceData;
+
+%% Select best sequences
+write_multichannelDecorrelator
+
+%% create plots
+plot_coherenceAll
+plot_exponentialDecayCurve
+plot_integerSPSerror
+plot_meanMagnitudeAll
+
